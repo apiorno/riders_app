3220d15013b8c2b24c22cb697265c7158b0d7fad
Alvaro Piorno <alvaropiorno100@gmail.com>
2022-08-01 01:04:05 -0300

    Update globals.dart

--- lib/globals.dart
+++ lib/globals.dart
@@ -3,5 +3,5 @@ import 'package:riders_app/models/rider.dart';
 
 final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 User? currentFirebaseUser;
-String mapKey = 'AIzaSyC0BIo10KL6XYEnqiNDSj0U6qLSOH53BjQ';
+String mapKey = 'MyApiKey';
 Rider? currentRider;
